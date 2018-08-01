cdb17f71038f41dcff059a9f1c2af62a2a84a930
KoTuKJuBoTuK <simonovantond@gmail.com>
2018-08-01 22:04:50 +0300

    Added BurgerIngridient Component

---
+++ src/components/Burger/BurgerIngridient/BurgerIngridient.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import classes from "./114 BurgerIngredient.css"
+
+const burgerIngridient = ( props ) => {
+    switch( props.type ) {
+        case ( ' bread-bottom ' ): 
+            return <div className={classes.BreadBottom}></div>
+        case ( 'bread-top' ):
+            return (
+                <div className={classes.BreadTop}>
+                    <div className={classes.Seeds1}></div>
+                    <div className={classes.Seeds2}></div>
+                </div>
+            );
+        case ( ' meat ' ):
+            return <div className={classes.Meat}></div>
+        case ( ' beacon ' ):
+            return <div className={classes.Beacon}></div>
+        case ( ' cheese ' ):
+            return <div className={classes.Cheese}></div>
+        case ( ' salad ' ):
+            return <div className={classes.Salad}></div>
+        default: 
+            return null;
+    }
+}
+
+export default burgerIngridient;
